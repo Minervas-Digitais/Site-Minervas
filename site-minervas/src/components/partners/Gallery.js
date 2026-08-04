d01bf1242629d4ea801077241063c53fb68fdeff
Vinicius Alves <viniciusg.alves03@gmail.com>
2026-08-04 15:34:18 -0300

    fix:conserta scroll parcerias realizadas

--- site-minervas/src/components/partners/Gallery.js
+++ site-minervas/src/components/partners/Gallery.js
@@ -50,45 +50,54 @@ class Carousel extends React.Component {
   
 
   render() {
-    var settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 3200,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          }
-        },
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-          }
-        },
-        {
-          breakpoint: 960,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
+var settings = {
+  dots: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  initialSlide: 0,
+  swipe: false,       // desativado por padrão (telas >= 961px)
+  draggable: false,
+  touchMove: false,
+  responsive: [
+    {
+      breakpoint: 3200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      }
+    },
+    {
+      breakpoint: 960,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        swipe: true,       // reativa abaixo de 960px
+        draggable: true,
+        touchMove: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipe: true,
+        draggable: true,
+        touchMove: true,
+      }
+    }
+  ]
+};
     
     return (
       <div className='carrossel'>
