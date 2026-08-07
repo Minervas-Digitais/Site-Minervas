23c6add81c36f5cb8c22ab561a92e307912632c1
thais-alves-silva <ta2376983@gmail.com>
2026-08-07 14:59:33 -0300

    Update Who.js
    Descritivo e foto do Minerv@s atualizada.

--- site-minervas/src/components/who/Who.js
+++ site-minervas/src/components/who/Who.js
@@ -1,7 +1,7 @@
 import React, {useRef} from "react";
 import exampleimage from "../../assets/example-image.svg";
 import Text from "./Text";
-import photo from "../../assets/group-photo.jpg"
+import photo from "../../assets/group-photo-novo.jpg"
 import "./Content.scss";
 
 function Who() {
@@ -18,7 +18,7 @@ function Who() {
           <Text click={() => handleClick()}/>
         </div>
         <div className="who-conteudo-direito">
-          <img src={photo} className="image-who" alt="imagem de quem é as minervas" />
+          <img src={photo} className="image-who" alt="Membros do projeto de extensão Minerv@s Digitais em encontro." />
         </div>
       </div>
     </div>
